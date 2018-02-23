776a2c3135d68f15f8cb9c427f73da516f8f3170
hqtran <hqtran@ucsd.edu>
2018-02-24 00:02:20 +0900

    First commit, has some basic files

---
+++ create.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<title>Create new Listing</title>
+</head>
+<body>
+  <h1>Navigation</h1>
+  <ul>
+    <li><a href="create.php">Create Listing</a></li>
+    <li><a href="profile.php">View Profile</a></li>
+    <li><a href="viewList.php">Join a Ride</a></li>
+  </ul>
+  <h2>Create New Listing</h2>
+</body>
