36c81920ecc4f10e1860de306ed9ae0d95f02836
hqtran <hqtran@ucsd.edu>
2018-02-28 02:08:45 +0900

    working on create

--- create.php
+++ create.php
@@ -17,11 +17,30 @@
       <li>Trip Title:</li>
       <!--here add username field based on user-->
       <li><input type="text" name="adname" /></li>
-      <li>Advertisement location</li>
-      <li><input type= "text" name = "adloc"/></li>
-      <li>Last Updated:</li>
-      <li><input type="text" name="addate" /></li>
+      <li>Start location</li>
+      <li><input type= "text" name = "start_loc"/></li>
+      <li>End location</li>
+      <li><input type= "text" name = "end_loc"/></li>
       <li><input type="submit" name="submit" /></li>
   </ul>
+  <?php
+    // Connect to the database. Please change the password in the following line accordingly
+    $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234"); 
+    $result = pg_query($db, "SELECT * FROM ads where ad_id = '$_POST[adid]'");    // Query template
+    $row    = pg_fetch_assoc($result);    // To store the result row
+    $date   = getdate(); //get the current date. Used when new listing is created.
+    if (isset($_POST['submit'])) { // Submit the update SQL command
+        $result = pg_query($db, "INSERT ads SET ad_id = '$_POST[adid_updated]',  
+    name = '$_POST[ad_name_updated]',price = '$_POST[price_updated]',  
+    date_of_publication = '$_POST[dop_updated]', 
+    start_loc = '$_POST[start_loc_updated]',
+    end_loc = '$_POST[end_loc_updated]'");
+        if (!$result) {
+            echo "Update failed!!";
+        } else {
+            echo "Update successful!";
+        }
+    }
+    ?>  
 </body>
 </html>
