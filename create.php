906e6c7b18fd89554cebbd699affa5b985c950ba
hqtran <hqtran@ucsd.edu>
2018-02-28 22:21:32 +0900

    Insert works for generic, need to get it working on submit/with variables. Date should also be working

--- create.php
+++ create.php
@@ -13,34 +13,59 @@
   </ul>
   <h2>Create New Listing</h2>
   <ul>
-      <li>Create New Listing</li>
-      <li>Trip Title:</li>
-      <!--here add username field based on user-->
+      <li>Username</li>
+      <!--here replace with username based on user-->
+      <li><input type="text" name="username" /></li>
+      <li>Trip Title</li>
       <li><input type="text" name="adname" /></li>
       <li>Start location</li>
       <li><input type= "text" name = "start_loc"/></li>
       <li>End location</li>
       <li><input type= "text" name = "end_loc"/></li>
+      <li>Price</li>
+      <li><input type= "number" name = "price"/></li>
       <li><input type="submit" name="submit" /></li>
   </ul>
   <?php
     // Connect to the database. Please change the password in the following line accordingly
     $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234"); 
-    $result = pg_query($db, "SELECT * FROM ads where ad_id = '$_POST[adid]'");    // Query template
-    $row    = pg_fetch_assoc($result);    // To store the result row
-    $date   = getdate(); //get the current date. Used when new listing is created.
-    if (isset($_POST['submit'])) { // Submit the update SQL command
-        $result = pg_query($db, "INSERT ads SET ad_id = '$_POST[adid_updated]',  
-    name = '$_POST[ad_name_updated]',price = '$_POST[price_updated]',  
-    date_of_publication = '$_POST[dop_updated]', 
-    start_loc = '$_POST[start_loc_updated]',
-    end_loc = '$_POST[end_loc_updated]'");
-        if (!$result) {
-            echo "Update failed!!";
-        } else {
-            echo "Update successful!";
-        }
+    //$row    = pg_fetch_assoc($result);    // To store the result row
+    if($db) {
+      echo "database connected";
     }
+    else {
+      echo "problem with database connection";
+    }
+    $date   = getdate(); //get the current date. Used when new listing is created.
+    if(!$date) {
+      echo "Date isn't set";
+    }
+    else {
+      echo "Date is set, it is " . "$date[0]";
+    }
+    if(isset($_POST['submit'])) {
+      echo "Submit is set";
+    }
+    else {
+      echo "Submit not set";
+    }
+    //if (isset($_POST['submit'])) { // Submit the update SQL command
+        $result = pg_query($db, /*"INSERT INTO ads (ad_id, username, name, price, date_of_publication, start_loc, end_loc) 
+          VALUES (
+    '$_POST[username]'.'$date[0]',  
+    '$_POST[username]',
+    '$_POST[ad_name]', $_POST[price],  
+    '$date[year]'.'-'.'$date[mon]'.'-'.'$date[mday]', 
+    '$_POST[start_loc]',
+    '$_POST[end_loc]')"*/
+        "INSERT INTO ads(ad_id, name, price, date_of_publication, start_loc, end_loc)
+VALUES ('101', 'Drive to Capitol', 10, '2018-2-17', 'Home', 'Capitol')");
+        if (!$result) {
+            echo "Create failed!!";
+        } else {
+            echo "Create successful!";
+        }
+    //}
     ?>  
 </body>
 </html>
