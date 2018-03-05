a543b4d145b67cfcbf3bd92551ce466f19e09a7f
hqtran <hqtran@ucsd.edu>
2018-03-05 21:58:16 +0800

    search somewhat functional, errors in seats and date/time

--- App/search.php
+++ App/search.php
@@ -19,18 +19,18 @@
   </div>
 </nav>-->
   <h2> Join Listing:</h2>
-  <ul><form name = 'search' action='http://localhost/App.search.php' method='POST'>
+  <ul><form name = 'search' action='http://localhost/demo/App/search.php' method='POST'>
     <li>Find Listings By:</li>
-      <li>Trip Title:</li>
+      <li>Owner:</li>
       <li><input type="text" name="adname" /></li>
       <li>Depart From:</li>
       <li><input type= "text" name = "adstartloc"/></li>
       <li>Going to:</li>
       <li><input type= "text" name = "adendloc"/></li>
       <li>Date:</li>
-      <li><input type= "date" name = "date"/></li>
+      <li><input type= "text" name = "date" placeholder="YYYY-MM-DD"/></li>
       <li>Departure Time:</li>
-      <li><input type= "time" name = "starttime"/></li>
+      <li><input type= "text" name = "starttime" placeholder = "HH:MM:SS"/></li>
       <li>Required Seats:</li>
       <li><input type= "number" name = "seats"/></li>
       <li><input type="submit" name="submit" /></li>
@@ -41,23 +41,37 @@
       // Connect to the database. Please change the password in the following line accordingly
       $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");	
       if ($db){
+        echo $_POST[adname];
+        echo $_POST[seats];
+        echo $_POST[adstartloc];
+        echo $_POST[adendloc];
+        echo $_POST[date];
+        echo $_POST[starttime];
         $result = pg_query($db, 
                   "SELECT * FROM User_Post 
                   WHERE Owner = '$_POST[adname]' 
-                  OR Seats >= '$_POST[seats]'
+                  /*OR Seats >= $_POST[seats]*/
                   OR Start = '$_POST[adstartloc]'
-                  OR End = '$_POST[adendloc]'
-                  OR depDate = '$_POST[date]'
-                  OR depTime = '$_POST[starttime]'
-                  ORDER BY ad_id ASC LIMIT 20");		
+                  OR Dest = '$_POST[adendloc]'
+                  /*OR depDate = '$_POST[date]'
+                  OR depTime = '$_POST[starttime]'*/
+                  ORDER BY Owner ASC LIMIT 20");		
         if (isset($_POST['submit'])) {
+          if($result) {
+            echo "Select Found";
+            //echo '$result';
+          }
+          else {
+            echo "Select not found";
+          }
           while ($row = pg_fetch_array($result)) { 
-            echo "<a href='http://localhost/listing.php'> 
+
+            echo "<a href='http://localhost/demo/Listings/profile.php?Owner=$row[Owner]&Seats=$row[Seats]&Start=$row[Start]&Dest=$row[Dest]&depDate=$row[depDate]&depTime=$row[depTime]'> 
               <ul>   
                 <li>Advertisement Name: '$row[Owner]'</li>
                 <li>Seats: '$row[Seats]'<\li>     
                 <li>Begin Location: '$row[Start]'</li>
-                <li>End Location: '$row[End]'</li>
+                <li>End Location: '$row[Dest]'</li>
                 <li>Departure Date: '$row[depDate]'</li>
                 <li>Departure Time: '$row[depTime]'</li>
               </ul>
--- Listings/listings.php
+++ Listings/listings.php
@@ -1,8 +1,3 @@
-<!-- When the user selects a listing, they should be given this page.
-	If they are the creator of the page, they should be able to edit fields and submit them to 
-	UPDATE the listing-->
-<!-- If they are not the creator, they should be able to bid on the listing-->
-<!-- If they are an administrator, any other restrictions should be removed-->	
 <!DOCTYPE html>
 <head>
 	<title>Create new Listing</title>
