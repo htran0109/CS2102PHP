5e473e87bcf6d757bde1536b0668d2ee0f19362c
Edward <edtuwork@gmail.com>
2018-03-25 19:37:41 +0800

    modified navbar code in Listings

--- Listings/profile.php
+++ Listings/profile.php
@@ -28,12 +28,12 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <ul class="nav navbar-nav navbar-left">
-      <li><a href="http://localhost/demo/Listings/new.php">Create Listing</a></li>
-      <li><a href="http://localhost/demo/Listings/profile.php">View My Listings</a></li>
-      <li class="active"><a href="http://localhost/demo/App/search.php">Join a Ride</a></li>
+      <li><a href="/demo/Listings/new.php">Create Listing</a></li>
+      <li><a href="/demo/Listings/profile.php">View My Listings</a></li>
+      <li class="active"><a href="/demo/App/search.php">Join a Ride</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="http://localhost/Users/index.php">My Profile</a></li>
+      <li><a href="/demo/Users/index.php">My Profile</a></li>
     </ul>
   </div>
 </nav>
