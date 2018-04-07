a18dde388b97205760062538b8e40541a688d693
Edward <edtuwork@gmail.com>
2018-04-07 16:31:27 +0800

    fixed code formatting

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>  
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  <style>li {list-style: none;}</style>
@@ -7,37 +6,39 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a class="navbar-brand" href="/demo/index.php">
-            <img alt="Brand" src="carpull.jpg" width="150" height="50">
-        </a>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="/demo/index.php">
+      <img alt="Brand" src="carpull.jpg" width="150" height="50">
+    </a>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExample02">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/demo/App/search.php">Join Ride</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/demo/Listings/new.php">Create Listing</a>
-          </li>
-        </ul>
-      </div>
-
-      <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-        <li class="nav-item dropdown">
-          <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            My Account
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
-            <a class="dropdown-item" href="/demo/Users/index.php">Profile</a>
-            <a class="dropdown-item" href="/demo/Listings/index.php">Listings</a>
-            <a class="dropdown-item" href="https://getbootstrap.com/docs/3.3/">Rides</a> <!--No Link Yet-->
-            <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Log Out</a>
-          </div>
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/demo/App/search.php">Join Ride</a>
         </li>
-    </nav>
+        <li class="nav-item">
+          <a class="nav-link" href="/demo/Listings/new.php">Create Listing</a>
+        </li>
+      </ul>
+    </div>
+
+    <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+      <li class="nav-item dropdown">
+        <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          My Account
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
+          <a class="dropdown-item" href="/demo/Users/index.php">Profile</a>
+          <a class="dropdown-item" href="/demo/Listings/index.php">Listings</a>
+          <a class="dropdown-item" href="https://getbootstrap.com/docs/3.3/">Rides</a> <!--No Link Yet-->
+          <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Log Out</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</body>
