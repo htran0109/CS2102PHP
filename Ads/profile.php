079ca5ea694bbb90da6de6ce7cc9acf98f5277f1
ChanMW96 <fateaverancus@gmail.com>
2018-04-07 21:17:06 +0800

    populate bids profile

--- Ads/profile.php
+++ Ads/profile.php
@@ -78,7 +78,21 @@
     		<input visibility: hidden name='deptime' value = <?php echo $ad['deptime']; ?>>
 			<button name="Edit" type="submit" class="btn btn-primary" style="margin-top:10px">Edit</button>
 		</form>
-	</div>
+		<?PHP
+
+			$bidder = $_SESSION["username"];
+			$customers = $_POST['customers'];
+				
+			if ((int)$customers > (int)$seats) {
+				echo "<script type='text/javascript'> 
+					document.getElementById('errorMessage').innerHTML = 'Not enough seats available.';
+				</script>";
+			}
+			
+			pg_query($db, "INSERT INTO Bid(Bidder, Owner, Start, Dest, depDate, depTime, Customers) VALUES('$bidder', '$owner', '$start', '$dest', '$depdate', '$deptime', '$customers');"); 
+			
+		?>
+
 
 </body>
 </html>
