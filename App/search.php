93f25aa2a9485be7fe61c8a28b9e0fd33be86787
Cherry Goh <cherrycecegoh@gmail.com>
2018-04-07 21:18:05 +0800

    no message

--- App/search.php
+++ App/search.php
@@ -42,112 +42,137 @@
 				<input name="adstartloc" type="text" class="form-control" placeholder="Enter starting location." />
 				<label for="adendloc">Destination</label>
 				<input name="adendloc" type="text" class="form-control" placeholder="Enter destination." />
-				<label for="sdate">Departure Date</label>
-				<input name="sdate" type="date" class="form-control" placeholder="Enter departure date." />
-				<label for="stime">Departure Time</label>
-				<input name="stime" type="time" class="form-control" placeholder="Enter departure time." />
-				<label for="edate">Arrival Date</label>
-				<input name="edate" type="date" class="form-control" placeholder="Enter arrival date." />
-				<label for="etime">Arrival Time</label>
-				<input name="etime" type="time" class="form-control" placeholder="Enter arrival time." />
+				<label> Range of dates accepted </label>
+				
+				<div class="row">
+					<div class="col-sm">
+						<input name="sdate" type="date" class="form-control" placeholder="Enter start date." />
+					</div>
+					<div class="col-sm">
+						<input name="edate" type="date" class="form-control" placeholder="Enter end date." />
+					</div>
+				</div>
+				
+				<label>Range of times accepted</label>
+				<div class="row">
+					<div class="col-sm">
+						<input name="stime" type="time" class="form-control" placeholder="Enter start time." />
+					</div>
+					<div class="col-sm">
+						<input name="etime" type="time" class="form-control" placeholder="Enter end time." />
+					</div>
+				</div>
 				<label for="number">Required Seats</label>
 				<input name="number" type="number" class="form-control" placeholder="Enter required seats." />
-				<br />
+				<p style="color:red">
+					<?php
+						try {
+							search();
+						} catch (Exception $e) {
+							echo $e->getMessage() . " Please try again.";
+						}
+						
+						function search() {
+							// Connect to the database. Please change the password in the following line accordingly
+						  $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234"); 
+						  if ($db){
+							$keys[] = (isset($_POST[adname])) ? $_POST[adname] : null;
+							$keys[] = (isset($_POST[adstartloc])) ? $_POST[adstartloc] : null;
+							$keys[] = (isset($_POST[adendloc])) ? $_POST[adendloc] : null;
+							$keys[] = (isset($_POST[sdate])) ? $_POST[sdate] : null;
+							$keys[] = (isset($_POST[edate])) ? $_POST[edate] : null;
+							$keys[] = (isset($_POST[stime])) ? $_POST[stime] : null;
+							$keys[] = (isset($_POST[etime])) ? $_POST[etime] : null;
+							$keys[] = (isset($_POST[seats])) ? $_POST[seats] : null;
+							
+							if (isset($_POST[sdate]) != isset($_POST[edate])) { throw new exception("You must input both start date and end date."); }
+							if (isset($_POST[stime]) != isset($_POST[etime])) { throw new exception("You must input both start time and end time."); }
+							$filters = "";
+							foreach ($keys as $id => $value) {
+							  if ($value == null) { continue; }
+							  if ($filters != "" && $id != 4 && $id != 6 ) { $filters.=' OR '; }
+							  switch ($id) {
+								case 0:
+								  $relevance .= " (case when Owner = '$_POST[adname]'  then 1 else 0 end) +";
+								  $filters .= " owner = '$_POST[adname]'";
+								  break;
+
+								case 1:
+								  $relevance .= " (case when start = '$_POST[adstartloc]'  then 1 else 0 end) +";
+								  $filters.=" start = '$_POST[adstartloc]'";
+								  break;
+
+								case 2:
+								  $relevance .= " (case when dest = '$_POST[adendloc]'  then 1 else 0 end) +";
+								  $filters.=" Dest = '$_POST[adendloc]'";
+								  break;
+
+								case 3:
+								  $relevance .= " (case when depDate BETWEEN '$_POST[sdate]'";
+								  $filters.=" depdate BETWEEN '$_POST[sdate]'";
+								  break;
+
+								case 4:
+								  $relevance .= " AND '$_POST[edate]' then 1 else 0 end) +";
+								  $filters.=" AND '$_POST[edate]'";
+								  break;
+
+								case 5:
+								  $relevance .= " (case when deptime BETWEEN '$_POST[stime]'";
+								  $filters.=" deptime BETWEEN '$_POST[stime]'";
+								  break;
+
+								case 6:
+								  $relevance .= " AND '$_POST[etime]' then 1 else 0 end) +";
+								  $filters.=" AND '$_POST[etime]'";
+								  break;
+
+
+								case 7:
+								  $relevance .= " (case when seats >= '$_POST[seats]' then 1 else 0 end) +";
+								  $filters.=" seats >= '$_POST[seats]'";
+								  break;
+							  }
+							}
+
+							$relevance = rtrim($relevance, " +");
+							$query = "select *," . $relevance . " as relevance FROM User_Post WHERE " . $filters . " ORDER BY relevance DESC";
+							$result = pg_query($db, $query);      
+							if (isset($_POST['submit'])) {
+							  if($result) {
+								echo "Select Found";
+								//echo '$result';
+							  }
+							  else {
+								echo "Select not found";
+							  }
+							  while ($row = pg_fetch_array($result)) { 
+								echo $row["owner"];
+
+								echo "<a href='http://localhost/demo/Ads/profile.php?Owner=$row[owner]&Seats=$row[seats]&Start=$row[start]&Dest=$row[dest]&depDate=$row[depdate]&depTime=$row[deptime]'> 
+								  <ul>   
+									<li>Advertisement Name: $row[owner]</li>
+									<li>Seats: $row[seats]</li>     
+									<li>Begin Location: $row[start]</li>
+									<li>End Location: $row[dest]</li>
+									<li>Departure Date: $row[depdate]</li>
+									<li>Departure Time: $row[deptime]</li>
+								  </ul>
+								</a>";
+							  }
+							} 
+						  } else {
+							echo "Connection failed";
+						  }
+						}
+					  ?>
+				</p>
 				<button name="submit" type="submit" class="btn btn-primary">Submit</button>
 			</div>
 		</form>
 	</div>
   </ul>
 
-    <?php
-      // Connect to the database. Please change the password in the following line accordingly
-      $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234"); 
-      if ($db){
-        $keys[] = (isset($_POST[adname])) ? $_POST[adname] : null;
-        $keys[] = (isset($_POST[adstartloc])) ? $_POST[adstartloc] : null;
-        $keys[] = (isset($_POST[adendloc])) ? $_POST[adendloc] : null;
-        $keys[] = (isset($_POST[sdate])) ? $_POST[sdate] : null;
-        $keys[] = (isset($_POST[edate])) ? $_POST[edate] : null;
-        $keys[] = (isset($_POST[stime])) ? $_POST[stime] : null;
-        $keys[] = (isset($_POST[etime])) ? $_POST[etime] : null;
-        $keys[] = (isset($_POST[seats])) ? $_POST[seats] : null;
-        $filters = "";
-        foreach ($keys as $id => $value) {
-          if ($value == null) { continue; }
-          if ($filters != "" && $id != 4 && $id != 6 ) { $filters.=' OR '; }
-          switch ($id) {
-            case 0:
-              $relevance .= " (case when Owner = '$_POST[adname]'  then 1 else 0 end) +";
-              $filters .= " owner = '$_POST[adname]'";
-              break;
-
-            case 1:
-              $relevance .= " (case when start = '$_POST[adstartloc]'  then 1 else 0 end) +";
-              $filters.=" start = '$_POST[adstartloc]'";
-              break;
-
-            case 2:
-              $relevance .= " (case when dest = '$_POST[adendloc]'  then 1 else 0 end) +";
-              $filters.=" Dest = '$_POST[adendloc]'";
-              break;
-
-            case 3:
-              $relevance .= " (case when depDate BETWEEN '$_POST[sdate]'";
-              $filters.=" depdate BETWEEN '$_POST[sdate]'";
-              break;
-
-            case 4:
-              $relevance .= " AND '$_POST[edate]' then 1 else 0 end) +";
-              $filters.=" AND '$_POST[edate]'";
-              break;
-
-            case 5:
-              $relevance .= " (case when deptime BETWEEN '$_POST[stime]'";
-              $filters.=" deptime BETWEEN '$_POST[stime]'";
-              break;
-
-            case 6:
-              $relevance .= " AND '$_POST[etime]' then 1 else 0 end) +";
-              $filters.=" AND '$_POST[etime]'";
-              break;
-
-
-            case 7:
-              $relevance .= " (case when seats >= '$_POST[seats]' then 1 else 0 end) +";
-              $filters.=" seats >= '$_POST[seats]'";
-              break;
-          }
-        }
-
-        $relevance = rtrim($relevance, " +");
-        $query = "select *," . $relevance . " as relevance FROM User_Post WHERE " . $filters . " ORDER BY relevance DESC";
-        $result = pg_query($db, $query);      
-        if (isset($_POST['submit'])) {
-          if($result) {
-            echo "Select Found";
-            //echo '$result';
-          }
-          else {
-            echo "Select not found";
-          }
-          while ($row = pg_fetch_array($result)) { 
-            echo $row["owner"];
-
-            echo "<a href='http://localhost/demo/Ads/profile.php?Owner=$row[owner]&Seats=$row[seats]&Start=$row[start]&Dest=$row[dest]&depDate=$row[depdate]&depTime=$row[deptime]'> 
-              <ul>   
-                <li>Advertisement Name: $row[owner]</li>
-                <li>Seats: $row[seats]</li>     
-                <li>Begin Location: $row[start]</li>
-                <li>End Location: $row[dest]</li>
-                <li>Departure Date: $row[depdate]</li>
-                <li>Departure Time: $row[deptime]</li>
-              </ul>
-            </a>";
-          }
-        } 
-      } else {
-        echo "Connection failed";
-      }
-      ?>
+    
 </body>
 </html>
