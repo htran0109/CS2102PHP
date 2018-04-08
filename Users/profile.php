3a9ce6c572450c735e89289e0d22e679d1242580
Cherry Goh <cherrycecegoh@gmail.com>
2018-04-08 12:36:49 +0800

    no message

--- Users/profile.php
+++ Users/profile.php
@@ -22,29 +22,10 @@
 </head>
 <body>
 	<?php
-<<<<<<< HEAD
 		include_once('../header.php');
 	?>
-	<div class="container">
-=======
-	include_once('../header.php');
-	?>
-	<!--<nav class="navbar navbar-default">
-	  <div class="container-fluid">
-		<ul class="nav navbar-nav navbar-left">
-		  <li><a href="http://localhost/Listings/new.php">Create Listing</a></li>
-		  <li><a href="http://localhost/Listings/index.php">View My Listings</a></li>
-		  <li><a href="http://localhost/App/search.php">Join a Ride</a></li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-		  <li><a href="http://localhost/Users/index.php">My Profile</a></li>
-		  <li><a href="#">Log Out</a></li>
-		</ul>
-	  </div>
-	</nav>-->
 	<div class="container-fluid">
->>>>>>> a32ce774951321384f27bf469739e928243a7e4a
-		<h1 class="display-4"> My Profile </h1>
+		<h1 class="display-4"> <?php if (isset($_GET["Owner"])) echo $_GET["Owner"] . "'s"; else echo "My"; ?> Profile </h1>
 		<dl class="row">
 		  <dt class="col-sm-3">User Name</dt>
 		  <dd class="col-sm-9"> <?php echo $username; ?> </dd>
