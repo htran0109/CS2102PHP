1536554a573c1d0926192d3cce90ec0d6eb28aba
Edward <edtuwork@gmail.com>
2018-04-08 15:16:12 +0800

    updated relational schema

--- new_CREATE_TABLE.sql
+++ new_CREATE_TABLE.sql
@@ -1,41 +1,44 @@
 /*
-DROP TABLE Profile CASCADE;
-DROP TABLE OwnsCar CASCADE;
-DROP TABLE UserPost CASCADE;
-DROP TABLE UserBid CASCADE;
+DROP TABLE profile CASCADE;
+DROP TABLE car CASCADE;
+DROP TABLE post CASCADE;
+DROP TABLE bid CASCADE;
 */
 
 CREATE TABLE profile (
 	username varchar(20),
-	first_name varchar(20),
-	last_name varchar(20),
-	mobile_number varchar(20),
-	email varchar(40),
-	password varchar(20),
-  	admin boolean DEFAULT FALSE ,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	mobile_number varchar(20) NOT NULL,
+	email varchar(40) NOT NULL,
+	password varchar(20) NOT NULL,
+	birthday DATE NOT NULL, #update new_insert_table.sql
+  	admin boolean DEFAULT FALSE,
 	PRIMARY KEY (username)
 );
 
 CREATE TABLE car (
 	license_plate varchar(10),
-	total_seats int,
-	color varchar(20),
-	model varchar(20),
-	make varchar(20),
-	username varchar(20),
-	FOREIGN KEY (username) REFERENCES Profile(username),
+	total_seats int NOT NULL,
+	color varchar(20) NOT NULL,
+	model varchar(20) NOT NULL,
+	make varchar(20) NOT NULL,
+	username varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES profile(username),
 	PRIMARY KEY (license_plate)
 );
 
 CREATE TABLE post (
-	owner varchar(20), #consider changing this to car -> we can obtain user from car but not vice versa
-	seats_available int,
+	license_plate varchar(20),
+	owner varchar(20) NOT NULL,
+	seats_available int NOT NULL,
 	origin varchar(128),
 	destination varchar(128),
 	depart_date DATE,
 	depart_time TIME,
-	PRIMARY KEY (owner, origin, destination, depart_date, depart_time),
-	FOREIGN KEY (owner) REFERENCES Profile(username)
+	PRIMARY KEY (license_plate, origin, destination, depart_date, depart_time),
+	FOREIGN KEY (license_plate) REFERENCES car(license_plate),
+	FOREIGN KEY (owner) REFERENCES profile(username)
 );
 
 CREATE TABLE bid (
@@ -45,12 +48,12 @@ CREATE TABLE bid (
 	destination varchar(128),
 	depart_date DATE,
 	depart_time TIME,
-	seats_desired int,
+	seats_desired int NOT NULL,
 	driver_rating int CHECK (0 <= driver_rating AND driver_rating <= 5),
 	passenger_rating int CHECK (0 <= passenger_rating AND passenger_rating <= 5),
 	accepted boolean DEFAULT FALSE,
 	PRIMARY KEY (bidder, owner, origin, destination, depart_date, depart_time),
-	FOREIGN KEY (bidder) REFERENCES Profile(username),
+	FOREIGN KEY (bidder) REFERENCES profile(username),
 	FOREIGN KEY (owner, origin, destination, depart_date, depart_time)
-	REFERENCES UserPost(owner, origin, destination, depart_date, depart_time)
+	REFERENCES post(owner, origin, destination, depart_date, depart_time)
 );
