149be17508e389d1ec186503c21a5aefe5052a0f
Cherry Goh <cherrycecegoh@gmail.com>
2018-04-14 01:18:10 +0800

    new users can add cars

--- Ads/new.php
+++ Ads/new.php
@@ -1,59 +1,59 @@
 <?PHP
-	session_start();
-	
-	if (empty($_SESSION["username"])){
-		echo "<script type='text/javascript'> 
-		alert('You are not logged in. You will be redirected to the login page.');
-		window.location.href = 'index.php';
-		</script>";
-	}
-	$user = $_SESSION["username"];
-	$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");
+session_start();
+
+if (empty($_SESSION["username"])){
+	echo "<script type='text/javascript'>
+	alert('You are not logged in. You will be redirected to the login page.');
+	window.location.href = 'index.php';
+	</script>";
+}
+$user = $_SESSION["username"];
+$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");
 ?>
 
-<!DOCTYPE html>  
+<!DOCTYPE html>
 <head>
-  <title>Create New Advertisement</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  <style>li {list-style: none;}</style>
+	<title>Create New Advertisement</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  <style>li {list-style: none;}</style>
 </head>
 <body>
 
-<?php
-include_once('../header.php');
-?>
+	<?php
+	include_once('../header.php');
+	?>
 	<div class="container">
-	<form id="form" action="new.php" method="POST" >
-		<h1 class="display-4"> Create New Advertisement </h1>
-		<div class="form-group">
-			<label for="start">Starting Location</label>
-			<input name="start" type="text" class="form-control" placeholder="Enter your starting location" required />
-			<label for="dest">Destination</label>
-			<input name="dest" type="text" class="form-control" placeholder="Enter your destination" required />
-			<label for="depdate">Departure Date</label>
-			<input name="depdate" type="date" class="form-control" placeholder="Enter your departure date" required />
-			<label for="deptime">Departure Time</label>
-			<input name="deptime" type="time" class="form-control" placeholder="Time" required />
-			<label for="seats">Number of Seats</label>
-			<input name="seats" type="number" class="form-control" placeholder="Enter the maximum number of seats available" required />
-			<label for="car">Car</label>
-			<select class="custom-select" name="car" form="form">
-				<?php 
+		<form id="form" action="new.php" method="POST" >
+			<h1 class="display-4"> Create New Advertisement </h1>
+			<div class="form-group">
+				<label for="start">Starting Location</label>
+				<input name="start" type="text" class="form-control" placeholder="Enter your starting location" required />
+				<label for="dest">Destination</label>
+				<input name="dest" type="text" class="form-control" placeholder="Enter your destination" required />
+				<label for="depdate">Departure Date</label>
+				<input name="depdate" type="date" class="form-control" placeholder="Enter your departure date" required />
+				<label for="deptime">Departure Time</label>
+				<input name="deptime" type="time" class="form-control" placeholder="Time" required />
+				<label for="seats">Number of Seats</label>
+				<input name="seats" type="number" class="form-control" placeholder="Enter the maximum number of seats available" required />
+				<label for="car">Car</label>
+				<select class="custom-select" name="car" form="form">
+					<?php
 					$carResults = pg_query($db, "Select * from car where username='$user';");
-					 while ($row = pg_fetch_array($carResults)) {
-						 echo "
-							<option value=$row[license_plate]> 
-							$row[license_plate] - $row[model] $row[make]
-							</option>
-						 ";
-					 }
-					
-				?>
-			</select>
-			
-			<p style="color:red">
-				<?php
-					
+					while ($row = pg_fetch_array($carResults)) {
+						echo "
+						<option value=$row[license_plate]>
+						$row[license_plate] - $row[model] $row[make]
+						</option>
+						";
+					}
+
+					?>
+				</select>
+
+				<p style="color:red">
+					<?php
+
 					if (isset($_POST['submit'])) {
 						try {
 							addBid();
@@ -63,9 +63,9 @@ include_once('../header.php');
 							echo $e->getMessage() . " Please try again.";
 						}
 					}
-					
+
 					function addBid() {
-						
+
 						$car = trim($_POST['car']);
 						$owner = $_SESSION['username'];
 						$start = trim($_POST['start']);
@@ -73,7 +73,7 @@ include_once('../header.php');
 						$depdate = trim($_POST['depdate']);
 						$deptime = trim($_POST['deptime']);
 						$seats = trim($_POST['seats']);
-						
+
 						// Connect to the database. Please change the password in the following line accordingly
 						$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");
 						$result = pg_query($db, "SELECT * FROM post;");
@@ -83,11 +83,11 @@ include_once('../header.php');
 							throw new exception("Operation failed.");
 						}
 					}
-				?>  
-			</p>
-		</div>
-		<button name="submit" type="submit" class="btn btn-primary">Submit</button>
-    </form>
+					?>
+				</p>
+			</div>
+			<button name="submit" type="submit" class="btn btn-primary">Submit</button>
+		</form>
 	</div>
 
 </body>
--- Users/new.php
+++ Users/new.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>  
+<!DOCTYPE html>
 <head>
   <title>Create New Account</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -24,9 +24,27 @@
 			<input name="emailaddress" type="email" class="form-control" placeholder="Enter your email" required />
 			<label for="birthdate">Birthdate</label>
 			<input name="birthdate" type="date" class="form-control" placeholder="Enter your birthdate" required />
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="ownCar" name="ownCar">
+        <label class="form-check-label" for="ownCar">
+          I own a car.
+        </label>
+      </div>
+      <div id = "carInfo" style="display:none">
+        <label for="license_plate">License Plate</label>
+  			<input name="license_plate" type="text" class="form-control" placeholder="Enter your car's license plate" />
+        <label for="model">Model</label>
+  			<input name="model" type="text" class="form-control" placeholder="Enter your car model e.g. Toyota, Honda, etc." />
+        <label for="make">Make</label>
+        <input name="make" type="text" class="form-control" placeholder="Enter your car make e.g. Corolla, Civic, etc." />
+        <label for="color">Color</label>
+        <input name="color" type="text" class="form-control" placeholder="Enter your car color" />
+        <label for="total_seats">Seats</label>
+        <input name="total_seats" type="number" class="form-control" placeholder="Enter total seats available" />
+      </div>
 			<p style="color:red">
 				<?php
-					
+
 					if (isset($_POST['submit'])) {
 						try {
 							addBid();
@@ -35,44 +53,67 @@
 							echo $e->getMessage() . " Please try again.";
 						}
 					}
-					
+
 					function addBid() {
-						
+
 						$username = trim($_POST['username']);
 						$password = trim($_POST['password']);
 						$mobilenumber = trim($_POST['mobilenumber']);
 						$emailaddress = trim($_POST['emailaddress']);
 						$firstname = trim($_POST['first_name']);
 						$lastname = trim($_POST['last_name']);
-						$birthdate = $_POST['birthdate'];
+						$birthdate = trim($_POST['birthdate']);
+
+            $license_plate = trim($_POST['license_plate']);
+            $model = trim($_POST['model']);
+            $make = trim($_POST['make']);
+            $color = trim($_POST['color']);
+            $total_seats = trim($_POST['total_seats']);
 						// Connect to the database. Please change the password in the following line accordingly
 
-						$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");	
+						$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");
 						if ($db) {
-							
 							$result = pg_query($db, "SELECT * FROM profile;");
 							pg_query($db, "INSERT INTO profile(first_name, last_name, username, password, mobile_number, email, birthday) VALUES('$firstname', '$lastname', '$username', '$password', '$mobilenumber', '$emailaddress', '$birthdate');");
 							$result1 = pg_query($db, "SELECT * FROM profile;");
-							if(pg_num_rows($result1) <= pg_num_rows($result)) {
-								throw new exception("Operation failed.");
 
-							}
-							else {
-								header("Location:../index");
-							}
+              if (pg_num_rows($result1) <= pg_num_rows($result)) {
+                throw new exception("Operation failed.");
+              }
+
+              if (isset($_POST['ownCar'])) {
+                $result2 = pg_query($db, "SELECT * FROM car;");
+                pg_query($db, "INSERT INTO car (license_plate, total_seats, color, model, make, username) VALUES('$license_plate', '$total_seats', '$color', '$model', '$make', '$username');");
+  							$result3 = pg_query($db, "SELECT * FROM car;");
+                if (pg_num_rows($result3) <= pg_num_rows($result2)) {
+                  throw new exception("Operation failed.");
+                }
+              }
+							header("Location:../index.php");
 						}
 						else {
 							throw new exception("Connection failed.");
 						}
 					}
-				?>  
+				?>
 			</p>
 		</div>
 		<button name="submit" type="submit" class="btn btn-primary">Submit</button>
 		<button name="back" type="submit" class="btn btn-primary"><a href="../index" style="color:white">Back</a></button>
 
-    	</form>
+  </form>
 	</div>
+
+  <script>
+  ownCar.addEventListener( 'change', function() {
+    if(this.checked) {
+        document.getElementById("carInfo").style.display="inline";
+    } else {
+        document.getElementById("carInfo").style.display="none";
+    }
+  });
+
+  </script>
 
 </body>
 </html>
